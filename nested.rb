ee6293e887590725bf1c9f52c34fc076090ecbf1
Ebo Lee <yiboli.coding@gmail.com>
2020-07-16 04:33:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,6 +85,10 @@ def adding_matz
         }
      }
 
+
+programmer_hash[:yukihiro_matsumoto][:known_for = "Ruby"]
+programmer_hash[:yukihiro_matsumoto][:languages = "LISP, C"]
+programmer_hash
     
 end
 
@@ -102,7 +111,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alans_kay][:alans_new_info] = "GUI"
      
+     puts programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +133,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+puts programmer_hash
 end
