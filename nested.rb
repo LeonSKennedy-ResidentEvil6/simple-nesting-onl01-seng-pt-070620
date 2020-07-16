3a0d4ddd5de2ce3c81edd9fc38277b97ced20a62
Ebo Lee <yiboli.coding@gmail.com>
2020-07-16 05:05:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,6 +135,7 @@ def adding_to_dennis
         }
      }
 
-new_language = programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-puts programmer_hash
+new_language = programmer_hash[:dennis_ritchie][:languages]
+new_language << "Assembly"
+programmer_hash
 end
